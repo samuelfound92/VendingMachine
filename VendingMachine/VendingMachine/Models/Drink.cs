c826b2178da79404171d5baf63d9a8561c17c145
Samuel Found <samuelfound@hotmail.co.uk>
2020-11-27 16:09:08 +0000

    Add Drink class

---
+++ VendingMachine/VendingMachine/Models/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Models
+{
+    public class Drink
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<string> Steps { get; set; }
+    }
+
+}
