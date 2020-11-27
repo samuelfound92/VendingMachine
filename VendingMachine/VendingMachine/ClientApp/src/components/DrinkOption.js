6bba6a5b5bb8d873278371b9ceb39cdb98fd1a0b
Samuel Found <samuelfound@hotmail.co.uk>
2020-11-27 17:54:34 +0000

    Load and display drink options from API

---
+++ VendingMachine/VendingMachine/ClientApp/src/components/DrinkOption.js
@@ -0,0 +1,9 @@
+﻿import React from 'react'
+
+export default function DrinkOption(props) {
+    return(
+        <div className='drink-option'>
+            <div>{props.drink.name}</div>
+        </div>
+    )
+}
