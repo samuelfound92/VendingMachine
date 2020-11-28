2b52691ee4729e3f6fa5a172e75e1ef0f5ac3d4f
Samuel Found <samuelfound@hotmail.co.uk>
2020-11-28 20:24:36 +0000

    Use parameterised constructors for Drink and DrinkOption classes

--- VendingMachine/VendingMachine/Data/DrinkRepo.cs
+++ VendingMachine/VendingMachine/Data/DrinkRepo.cs
@@ -10,22 +10,21 @@ namespace VendingMachine.Data
     {
         private static Drink[] Drinks = new[]
         {
-            new Drink()
-            {
-                ID = 1,
-                Name = "Lemon Tea",
-                Steps = new string[]{ "Boil some water", "Steep the water in the tea", "Pour tea in the cup", "Add lemon" }
-            },
-            new Drink(){
-                ID = 2,
-                Name = "Coffee",
-                Steps = new string[]{ "Boil some water", "Brew the coffee grounds", "Pour coffee in the cup", "Add sugar and milk" }
-            },
-            new Drink(){
-                ID = 3,
-                Name = "Chocolate",
-                Steps = new string[]{ "Boil some water", "Add drinking chocolate powder to the water", "Pour chocolate in the cup" }
-            }
+            new Drink(
+                1,
+                "Lemon Tea",
+                new string[]{ "Boil some water", "Steep the water in the tea", "Pour tea in the cup", "Add lemon" }
+            ),
+            new Drink(
+                2,
+                "Coffee",
+                new string[]{ "Boil some water", "Brew the coffee grounds", "Pour coffee in the cup", "Add sugar and milk" }
+            ),
+            new Drink(
+                3,
+                "Chocolate",
+                new string[]{ "Boil some water", "Add drinking chocolate powder to the water", "Pour chocolate in the cup" }
+            )
         };
 
         public Drink GetDrink(int id)
--- VendingMachine/VendingMachine/Models/Drink.cs
+++ VendingMachine/VendingMachine/Models/Drink.cs
@@ -7,12 +7,22 @@ namespace VendingMachine.Models
 {
     public class DrinkOption
     {
+        public DrinkOption(int id, string name) 
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+
         public int ID { get; set; }
         public string Name { get; set; }        
     }
 
     public class Drink : DrinkOption 
     {
+        public Drink(int id, string name, string[] steps) : base(id, name) 
+        {
+            this.Steps = steps;
+        }
         public string[] Steps { get; set; }
     }
 
